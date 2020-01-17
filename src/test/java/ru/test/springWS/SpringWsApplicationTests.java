ec1e62e14994337fe5cf9311c6d6dc185651cbd9
Vladimir <goncharov.saratov@gmail.com>
2020-01-17 21:32:44 +0400

    init

---
+++ src/test/java/ru/test/springWS/SpringWsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.springWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
